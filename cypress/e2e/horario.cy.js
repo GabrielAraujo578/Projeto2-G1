191799c31c609ae017f1afc19bb1b3cda684fa0c
AlineTakakura <aline.mtakakura@gmail.com>
2025-06-04 20:43:51 -0300

    Ajuste teste horários

--- cypress/e2e/horario.cy.js
+++ cypress/e2e/horario.cy.js
@@ -8,15 +8,15 @@ describe('Visualização de horário após matrícula', () => {
   it('aluno se matricula e vê a turma no horário correto', () => {
     cy.loginProfessor()
     cy.contains('Gerenciar Turmas').click()
-    cy.criarTurma(nomeTurma, 'Segunda-feira', '08:00', '10:00', 'Turma para teste de horário')
+    cy.criarTurma(nomeTurma, 'Terça-feira', '08:00', '10:00', 'Turma para teste de horário')
 
     cy.get('@codigoTurma').then((codigoTurma) => {
       cy.loginAluno()
       cy.contains('Minhas Turmas').click()
 
-      cy.get('i.fas.fa-plus').click()
+      cy.get('button.btn-matricular').should('be.visible').click()
       cy.get('input[name="codigo"]').type(codigoTurma)
-      cy.contains('Matricular').click()
+      cy.get('button.btn-acessar').contains('Matricular').click()
 
       cy.visit('http://localhost:8000/aluno/')
       cy.contains('Horário').click()
