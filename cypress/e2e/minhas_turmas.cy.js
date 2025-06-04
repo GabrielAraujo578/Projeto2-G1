86e280d3b004c0d36e8b94d975c5527861da06d4
AlineTakakura <aline.mtakakura@gmail.com>
2025-06-04 20:25:10 -0300

    Ajustes Testes

--- cypress/e2e/minhas_turmas.cy.js
+++ cypress/e2e/minhas_turmas.cy.js
@@ -1,63 +1,71 @@
 describe('aluno acessa turmas', () => {
-  const nomeTurma = `Turma Completa ${Date.now()}`
   const tituloConteudo = `Conteúdo Aula ${Date.now()}`
   const descricaoConteudo = 'Descrição do conteúdo para teste completo.'
+  let nomeTurma = ''
+  let codigoTurma = ''
 
-  it('professor cria turma, adiciona conteúdo e aluno se matricula', () => {
+  before(() => {
+    cy.deletedatabase()
+  })
+
+  beforeEach(() => {
+    nomeTurma = `Turma Completa ${Date.now()}`
+
     cy.loginProfessor()
     cy.contains('Gerenciar Turmas').click()
     cy.criarTurma(nomeTurma, 'Segunda-feira', '14:00', '16:00', 'Turma criada para teste completo')
 
-    cy.contains(nomeTurma).parent().within(() => {
+    cy.contains(nomeTurma).closest('.turma-card').within(() => {
+      cy.get('div.turma-codigo').invoke('text').then(codigo => {
+        codigoTurma = codigo.replace('Código: ', '').trim()
+      })
       cy.get('a.btn-gerenciar').click()
-      cy.get('div.turma-codigo').invoke('text').as('codigoTurma')
     })
 
+    cy.url().should('include', '/turma/')
     cy.adicionarConteudo(tituloConteudo, descricaoConteudo, 'cypress/fixtures/exemplo.pdf')
+  })
 
+  it('professor adiciona conteúdo e aluno se matricula', () => {
     cy.contains(tituloConteudo).should('exist')
     cy.contains(descricaoConteudo).should('exist')
 
     cy.loginAluno()
     cy.contains('Minhas Turmas').click()
-    cy.get('@codigoTurma').then(codigo => {
-      cy.get('i.fas.fa-plus').click()
-      cy.get('input[name="codigo"]').type(codigo)
-      cy.get('button.btn-acessar').contains('Matricular').click()
-    })
+    cy.get('button.btn-matricular').should('be.visible').click()
+    cy.get('input[name="codigo"]').type(codigoTurma)
+    cy.get('button.btn-acessar').contains('Matricular').click()
+
+    cy.url().should('include', '/aluno/')
+    cy.contains('Minhas Turmas').click()
 
     cy.contains(nomeTurma).should('exist')
-    cy.contains(nomeTurma).parent().within(() => {
+    cy.contains(nomeTurma).closest('.turma-card').within(() => {
       cy.get('a.btn-acessar').click()
     })
 
     cy.contains(tituloConteudo).should('exist')
     cy.contains(descricaoConteudo).should('exist')
-    cy.get('a').contains('Download').should('have.attr', 'href').and('include', 'exemplo.pdf')
+    cy.get('a').contains('Download')
+      .should('have.attr', 'href')
+      .and('include', 'exemplo')
   })
 
   it('não matricula com código errado e não mostra turma em Minhas Turmas', () => {
     cy.loginAluno()
     cy.contains('Minhas Turmas').click()
-
-    cy.get('i.fas.fa-plus').click()
+    cy.get('button.btn-matricular').should('be.visible').click()
     cy.get('input[name="codigo"]').type('codigoErrado123')
     cy.get('button.btn-acessar').contains('Matricular').click()
-
     cy.contains(nomeTurma).should('not.exist')
   })
 
   it('abre modal de matrícula e clica em cancelar, não se matricula', () => {
     cy.loginAluno()
     cy.contains('Minhas Turmas').click()
-
-    cy.get('i.fas.fa-plus').click()
-    cy.get('@codigoTurma').then(codigo => {
-      cy.get('input[name="codigo"]').type(codigo)
-
-      cy.get('button.btn-acessar').contains('Cancelar').click()
-    })
-
+    cy.get('button.btn-matricular').should('be.visible').click()
+    cy.get('input[name="codigo"]').type(codigoTurma)
+    cy.get('button.btn-acessar').contains('Cancelar').click()
     cy.contains(nomeTurma).should('not.exist')
   })
 })
